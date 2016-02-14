e36fe6be851652b4a980f7fccd69021d1966815f
aalmiray <aalmiray@gmail.com>
2016-02-14 13:47:25 +0100

    Define the contract for the application's Configuration

--- api/src/main/java/javax/application/Configuration.java
+++ api/src/main/java/javax/application/Configuration.java
@@ -19,5 +19,161 @@ package javax.application;
  * @author Andres Almiray
  */
 public interface Configuration {
+    /**
+     * Searches for the key in this configuration.
+     *
+     * @param key the key to search.
+     *
+     * @return true if the context (or its parent) contains the given key, false otherwise.
+     */
+    boolean containsKey(String key);
 
+    /**
+     * /**
+     * Finds a value associated with the given key. The value is
+     * blindly cast to type <tt>T</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    <T> T get(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * blindly cast to type <tt>T</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    <T> T get(String key, T defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>boolean</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    boolean getAsBoolean(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>boolean</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    boolean getAsBoolean(String key, boolean defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to an <tt>int</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    int getAsInt(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to an <tt>int</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    int getAsInt(String key, int defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>long</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    long getAsLong(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>long</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    long getAsLong(String key, long defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>float</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    float getAsFloat(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>float</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    float getAsFloat(String key, float defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>double</tt> if found.
+     *
+     * @param key the key to search.
+     */
+    double getAsDouble(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>double</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    double getAsDouble(String key, double defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>String</tt> if found.
+     *
+     * @param key the key to search.
+     */
+
+    String getAsString(String key);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to a <tt>String</tt> if found. If not found then the
+     * supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+
+    String getAsString(String key, String defaultValue);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to type <tt>T</tt> if found using a {@code Converter}.
+     *
+     * @param key  the key to search.
+     * @param type the type to be returned.
+     */
+    <T> T getConverted(String key, Class<T> type);
+
+    /**
+     * Finds a value associated with the given key. The value is
+     * converted to type <tt>T</tt> if found using a {@code Converter}.
+     * If not found then the supplied <tt>defaultValue</tt> will be returned.
+     *
+     * @param key          the key to search.
+     * @param type         the type to be returned.
+     * @param defaultValue the value to be returned if the key is not found.
+     */
+    <T> T getConverted(String key, Class<T> type, T defaultValue);
 }
